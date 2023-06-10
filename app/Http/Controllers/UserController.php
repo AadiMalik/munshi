15efd9378ae5ffc3f2999c77cf89ce5316514b4a
Adnan Junaid <adnanjunaid76279@gmail.com>
2023-06-10 16:54:07 +0500

    login user hide

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::where('id',Auth()->user()->id)->get();
         return view('users.index', compact('users'));
     }
 
